5048fb7e4ed2995d493d164450652bb938560291
Yulia-Kablukova <70965973+Yulia-Kablukova@users.noreply.github.com>
2021-04-19 23:16:16 +0700

    done

--- lab6.c
+++ lab6.c
@@ -3,19 +3,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/time.h>
 
 #define MAX_SIZE 100
 
 int main(int argc, char *argv[]) {
-
     if (argc != 2) {
-        printf("Wrong number of arguments");
+        printf("Wrong number of arguments\n");
         return 0;
     }
 
     int fd;
-
     if ((fd = open(argv[1], O_RDONLY)) == -1) {
         perror("Cannot open file");
         return 0;
@@ -26,14 +23,11 @@ int main(int argc, char *argv[]) {
     int line = 1;
     long offsets[MAX_SIZE];
     int lengths[MAX_SIZE];
-
     for (int i = 0; i < MAX_SIZE; i++) {
         lengths[i] = -1;
     }
-
     offsets[0] = 0L;
     offsets[1] = 0L;
-
     while (read(fd, &c, 1) > 0) {
         len++;
         if (c == '\n') {
@@ -48,50 +42,48 @@ int main(int argc, char *argv[]) {
 
     int line_number = 0;
     char buff[MAX_SIZE];
-    struct timeval start, end;
+    int fd2;
 
-    gettimeofday(&start, NULL);
+    if ((fd2 = open("/dev/tty", O_RDWR | O_NDELAY)) == -1) {
+        perror("/dev/tty");
+        exit(2);
+    }
 
-    while (printf("Line number (0 for exit): ") && scanf("%d", &line_number)) {
-        gettimeofday(&end, NULL);
+    while (1) {
+        printf("Line number (0 for exit): ");
+        fflush(stdout);
+        sleep(5);
 
-        if (end.tv_sec - start.tv_sec > 5) {
+        if (read(fd2, buff, BUFSIZ) == 0) {
             printf("Time limit exceeded!\n");
-            for (line_number = 1; line_number < line; line_number++) {
-                if (lseek(fd, offsets[line_number], SEEK_SET) == -1) {
-                    perror("lseek: ");
-                    break;
-                }
-                if (read(fd, buff, lengths[line_number]) <= 0) {
-                    perror("read: ");
-                    break;
-                }
-                buff[lengths[line_number]] = '\0';
-                printf("%s", buff);
+            lseek(fd,0,SEEK_SET);
+            while (read(fd, &c, 1) > 0) {
+                putchar(c);
             }
             break;
         }
 
+        line_number = atoi(buff);
+
         if (line_number == 0)
-            break;
+            return 0;
 
         if (line_number < 0 || line_number > MAX_SIZE || lengths[line_number] == -1) {
             printf("Wrong line number!\n");
         } else {
             if (lseek(fd, offsets[line_number], SEEK_SET) == -1) {
                 perror("lseek: ");
-                break;
+                return 0;
             }
             if (read(fd, buff, lengths[line_number]) <= 0) {
                 perror("read: ");
-                break;
+                return 0;
             }
             buff[lengths[line_number]] = '\0';
             printf("%s", buff);
         }
-
-        gettimeofday(&start, NULL);
     }
+
     close(fd);
-    return 0;
+    close(fd2);
 }
