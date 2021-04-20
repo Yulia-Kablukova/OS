328c68dc417b38cc6370240b921e59930c77f00f
Yulia-Kablukova <70965973+Yulia-Kablukova@users.noreply.github.com>
2021-04-20 10:20:20 +0700

    Update lab7.c

--- lab7.c
+++ lab7.c
@@ -1,6 +1,3 @@
-//  write(1, offsets[line_number], lengths[line_number]);
-        
-
 #include <fcntl.h>
 #include <sys/types.h>
 #include <stdio.h>
@@ -23,9 +20,8 @@ int main(int argc, char *argv[]) {
     }
 
     int size = lseek(fd, 0, SEEK_END);
-    char *pa = (char *) mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    char *pa = (char *) mmap(0, size, PROT_READ, MAP_SHARED, fd, 0);
 
-    char c;
     int len = 0;
     int line = 1;
     char* offsets[MAX_SIZE];
@@ -33,13 +29,11 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i < MAX_SIZE; i++) {
         lengths[i] = -1;
     }
-    offsets[0] = 0L;
-    offsets[1] = pa;
 
     for (int i = 0; i < size; i++) {
         if (*(pa + i) == '\n') {
-            lengths[i++] = len;
-            offsets[i] = pa + i + 1;
+            lengths[line++] = len;
+            offsets[line] = pa + i + 1;
             len = 0;
         } else {
             len++;
@@ -76,77 +70,10 @@ int main(int argc, char *argv[]) {
         if (line_number < 0 || line_number > MAX_SIZE || lengths[line_number] == -1) {
             printf("Wrong line number!\n");
         } else {
-            printf("%c", *(pa + offsets[line_number]));
-            /*if (lseek(fd, offsets[line_number], SEEK_SET) == -1) {
-                perror("lseek: ");
-                return 0;
-            }
-            if (read(fd, buff, lengths[line_number]) <= 0) {
-                perror("read: ");
-                return 0;
-            }
-            buff[lengths[line_number]] = '\0';
-            printf("%s", buff);*/
+            write(1, offsets[line_number], lengths[line_number]);
         }
     }
-
-            /*while (read(fd, &c, 1) > 0) {
-        len++;
-        if (c == '\n') {
-            lengths[line++] = len;
-            offsets[line] = lseek(fd, 0L, SEEK_CUR);
-            if (offsets[line] == -1) {
-                perror("lseek: ");
-            }
-            len = 0;
-        }*/
-    //}
-
-    /*int line_number = 0;
-    char buff[MAX_SIZE];
-    int fd2;
-
-    if ((fd2 = open("/dev/tty", O_RDWR | O_NDELAY)) == -1) {
-        perror("/dev/tty");
-        exit(2);
-    }
-
-    while (1) {
-        printf("Line number (0 for exit): ");
-        fflush(stdout);
-        sleep(5);
-
-        if (read(fd2, buff, BUFSIZ) == 0) {
-            printf("Time limit exceeded!\n");
-            lseek(fd,0,SEEK_SET);
-            while (read(fd, &c, 1) > 0) {
-                write(fd2, &c, 1);
-            }
-            break;
-        }
-
-        line_number = atoi(buff);
-
-        if (line_number == 0)
-            return 0;
-
-        if (line_number < 0 || line_number > MAX_SIZE || lengths[line_number] == -1) {
-            printf("Wrong line number!\n");
-        } else {
-            if (lseek(fd, offsets[line_number], SEEK_SET) == -1) {
-                perror("lseek: ");
-                return 0;
-            }
-            if (read(fd, buff, lengths[line_number]) <= 0) {
-                perror("read: ");
-                return 0;
-            }
-            buff[lengths[line_number]] = '\0';
-            printf("%s", buff);
-        }
-    }*/
 
     close(fd);
     close(fd2);
 }
-
