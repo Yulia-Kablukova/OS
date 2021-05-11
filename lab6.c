31c7e2816b47bf8c8076729652ac0d91ad3e9bba
Yulia-Kablukova <70965973+Yulia-Kablukova@users.noreply.github.com>
2021-05-11 09:55:51 +0700

    write instead of printf

--- lab6.c
+++ lab6.c
@@ -7,13 +7,14 @@
 
 #define MAX_SIZE 100
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     if (argc != 2) {
         printf("Wrong number of arguments\n");
         return 0;
     }
 
     int fd;
+
     if ((fd = open(argv[1], O_RDONLY)) == -1) {
         perror("Cannot open file");
         return 0;
@@ -24,13 +25,21 @@ int main(int argc, char* argv[]) {
     int line = 1;
     long offsets[MAX_SIZE];
     int lengths[MAX_SIZE];
+
     for (int i = 0; i < MAX_SIZE; i++) {
         lengths[i] = -1;
     }
+
     offsets[0] = 0L;
     offsets[1] = 0L;
+
     while (read(fd, &c, 1) > 0) {
         len++;
+        if (len == MAX_SIZE && c != '\n') {
+            char warningMessage[] = "Warning: too long line\n";
+            write(0, warningMessage, sizeof(warningMessage));
+            while (c != '\n' && read(fd, &c, 1) > 0) continue;
+        }
         if (c == '\n') {
             lengths[line++] = len;
             offsets[line] = lseek(fd, 0L, SEEK_CUR);
@@ -43,19 +52,23 @@ int main(int argc, char* argv[]) {
 
     int line_number = 0;
     char buff[MAX_SIZE];
-    int i;
     int flg;
     struct pollfd pfds[1];
+    char lineNumberMessage[] = "Line number (0 for exit): ";
 
     while (1) {
 
         pfds[0].fd = 0;
         pfds[0].events = POLLIN;
-        printf("Line number (0 for exit): ");
+
+        write(0, lineNumberMessage, sizeof(lineNumberMessage));
         fflush(stdout);
+
         flg = poll(pfds, 1, 5000);
+
         if (!flg) {
-            printf("Time limit exceeded!\n");
+            char str[] = "Time limit exceeded!\n";
+            write(0, str, sizeof(str));
             lseek(fd, 0, SEEK_SET);
             while (read(fd, &c, 1) > 0) {
                 write(0, &c, 1);
@@ -64,20 +77,21 @@ int main(int argc, char* argv[]) {
         }
 
         if (pfds[0].revents & POLLIN) {
-        i = read(0, buff, MAX_SIZE);
-        if (!i) {
-            printf("stdin closed\n");
-            return 0;
+            if (!read(0, buff, MAX_SIZE)) {
+                char str[] = "stdin closed\n";
+                write(0, str, sizeof(str));
+                return 0;
             }
         }
         line_number = atoi(buff);
-        if (line_number == 0)
+        if (line_number == 0) {
             return 0;
-    
-        if (line_number < 0 || line_number > MAX_SIZE || lengths[line_number] == -1) {
-            printf("Wrong line number!\n");
         }
-        else {
+
+        if (line_number < 0 || line_number > MAX_SIZE || lengths[line_number] == -1) {
+            char str[] = "Wrong line number!\n";
+            write(0, str, sizeof(str));
+        } else {
             if (lseek(fd, offsets[line_number], SEEK_SET) == -1) {
                 perror("lseek: ");
                 return 0;
@@ -87,7 +101,7 @@ int main(int argc, char* argv[]) {
                 return 0;
             }
             buff[lengths[line_number]] = '\0';
-            printf("%s", buff);
+            write(0, buff, lengths[line_number]);
         }
     }
 
