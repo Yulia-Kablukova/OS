0420f8d65dbc7811528a6b0192b46f7114a5176e
Yulia-Kablukova <70965973+Yulia-Kablukova@users.noreply.github.com>
2021-05-11 10:14:10 +0700

    printf to write

--- lab7.c
+++ lab7.c
@@ -8,24 +8,25 @@
 
 #define MAX_SIZE 100
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     if (argc != 2) {
         printf("Wrong number of arguments\n");
         return 0;
     }
 
     int fd;
+
     if ((fd = open(argv[1], O_RDONLY)) == -1) {
         perror("Cannot open file");
         return 0;
     }
 
     int size = lseek(fd, 0, SEEK_END);
-    char* pa = (char*)mmap(0, size, PROT_READ, MAP_SHARED, fd, 0);
+    char *pa = (char *) mmap(0, size, PROT_READ, MAP_SHARED, fd, 0);
 
     int len = 0;
     int line = 1;
-    char* offsets[MAX_SIZE];
+    char *offsets[MAX_SIZE];
     int lengths[MAX_SIZE];
     for (int i = 0; i < MAX_SIZE; i++) {
         lengths[i] = -1;
@@ -34,40 +35,49 @@ int main(int argc, char* argv[]) {
     offsets[1] = pa;
 
     for (int i = 0; i < size; i++) {
+        if (len == MAX_SIZE && *(pa + i) != '\n') {
+            char warningMessage[] = "Warning: too long line\n";
+            write(0, warningMessage, sizeof(warningMessage));
+            while (++i < size && *(pa + i) != '\n') continue;
+        }
         if (*(pa + i) == '\n') {
             lengths[line++] = len;
             offsets[line] = pa + i + 1;
             len = 0;
-        }
-        else {
+        } else {
             len++;
         }
     }
 
     int line_number = 0;
     char buff[MAX_SIZE];
-    int i;
     int flg;
     struct pollfd pfds[1];
+    char lineNumberMessage[] = "Line number (0 for exit): ";
 
     while (1) {
 
         pfds[0].fd = 0;
         pfds[0].events = POLLIN;
-        printf("Line number (0 for exit): ");
+
+        write(0, lineNumberMessage, sizeof(lineNumberMessage));
         fflush(stdout);
+
         flg = poll(pfds, 1, 5000);
+
         if (!flg) {
-            printf("Time limit exceeded!\n");
+            char str[] = "Time limit exceeded!\n";
+            write(0, str, sizeof(str));
             for (int i = 0; i < size; i++) {
-                printf("%c", *(pa + i));
+                write(0, pa + i, 1);
             }
             break;
         }
+
         if (pfds[0].revents & POLLIN) {
-            i = read(0, buff, MAX_SIZE);
-            if (!i) {
-                printf("stdin closed\n");
+            if (!read(0, buff, MAX_SIZE)) {
+                char str[] = "stdin closed\n";
+                write(0, str, sizeof(str));
                 return 0;
             }
         }
@@ -77,13 +87,15 @@ int main(int argc, char* argv[]) {
             return 0;
 
         if (line_number < 0 || line_number > MAX_SIZE || lengths[line_number] == -1) {
-            printf("Wrong line number!\n");
-        }
-        else {
+            char str[] = "Wrong line number!\n";
+            write(0, str, sizeof(str));
+        } else {
             write(1, offsets[line_number], lengths[line_number]);
-            printf("\n");
+            char str[] = "\n";
+            write(0, str, sizeof(str));
         }
     }
+
     munmap(0, size);
     close(fd);
 }
