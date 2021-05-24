6224a47bd44053c089885ac41c9ef38bcdcedabf
Yulia-Kablukova <70965973+Yulia-Kablukova@users.noreply.github.com>
2021-05-25 02:26:42 +0700

    Create lab26_2.c

---
+++ lab26_2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+
+    if(argc != 2) {
+        printf("bad input\n");
+        return 0;
+    }
+
+    FILE *file;
+
+    if ((file = popen(argv[1], "w")) == NULL) {
+        perror("open");
+    }
+
+    char line[20] = "HeLLo FrOM CHiLd 1\n";
+    char line2[20] = "Hello AGain !\n";
+
+    fputs(line, file);
+    fputs(line2, file);
+
+    if (pclose(file) == -1) {
+        perror("close");
+    }
+
+    return 0;
+}
